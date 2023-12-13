04149107de3e9ab967e86aea46e753f99e8de619
sergio Serbluk <sergiodserbluk@gmail.com>
2023-12-13 20:08:44 -0300

    rp

--- objetos2.py
+++ objetos2.py
@@ -1,12 +1,15 @@
 import datetime
 
 class Persona:
+    #defino los atributos
     __nombre=str
     __anioNac=int
     __edad=int
+    
     def __init__(self, nombre, anionacimiento):
         self.__nombre = nombre
         self.__anioNac = anionacimiento
+    
 
     def get_nombre(self):
         return self.__nombre
@@ -17,11 +20,7 @@ class Persona:
     def get_edad(self):
         year=datetime.datetime.now().year
         self.__edad=year- self.__anioNac
-        return self.__edad
-
-  
-
-  
+        return self.__edad 
 
 # Crear una instancia de la clase Persona
 persona = Persona("Juan", 1980)
@@ -29,14 +28,17 @@ persona = Persona("Juan", 1980)
 # Acceder a los atributos mediante los métodos get
 print(persona.get_nombre())  # Imprime: Juan
 print(persona.get_edad())    # Imprime: 
-
-# Intentar acceder directamente a los atributos privados genera un error
-#print(persona.__nombre)  # Genera un error
-
-# Cambiar los atributos mediante los métodos set
-persona.set_nombre("Juan Carlos")
-
-
-print(persona.get_nombre())  # Imprime: Carlos
-print(persona.get_edad())    # Imprime: 
+persona.set_nombre("Juan Manuel")
+print(persona.get_nombre())
+otrapersona=Persona("laura")
+print(otrapersona.get_nombre())
+## Intentar acceder directamente a los atributos privados genera un error
+##print(persona.__nombre)  # Genera un error
+#
+## Cambiar los atributos mediante los métodos set
+#persona.set_nombre("Juan Carlos")
+#
+#
+#print(persona.get_nombre())  # Imprime: Carlos
+#print(persona.get_edad())    # Imprime: 
 
